397eb4ce84be18100fef56a6c3af253ea1dfeb54
Jan Svantner <janci@janci.net>
2012-10-26 04:16:34 +0200

    add nette sandbox

---
+++ libs/nette/nette/tests/Nette/Database/connect.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Test: Nette\Database test boostap.
+ *
+ * @author     Jakub Vrana
+ * @author     Jan Skrasek
+ * @package    Nette\Database
+ */
+
+require __DIR__ . '/../bootstrap.php';
+
+
+$config = parse_ini_file(__DIR__ . '/databases.ini', TRUE);
+$current = isset($_SERVER['argv'][1]) ? $config[$_SERVER['argv'][1]] : reset($config);
+
+
+try {
+	$rc = new ReflectionClass('Nette\Database\Connection');
+	/** @var Nette\Database\Connection */
+	$connection = $rc->newInstanceArgs($current);
+
+} catch (PDOException $e) {
+	TestHelpers::skip("Connection to '$current[dsn]' failed. Reason: " . $e->getMessage());
+}
+
+TestHelpers::lock($current['dsn'], dirname(TEMP_DIR));
+
+unset($config, $current, $rc);
+$driverName = $connection->getAttribute(PDO::ATTR_DRIVER_NAME);
---
+++ libs/nette/nette/tests/Nette/Diagnostics/Debugger.Bar.003.phpt
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Test: Nette\Diagnostics\Debugger Bar in production mode.
+ *
+ * @author     David Grudl
+ * @package    Nette\Diagnostics
+ */
+
+use Nette\Diagnostics\Debugger;
+
+
+
+require __DIR__ . '/../bootstrap.php';
+
+
+
+Debugger::$consoleMode = FALSE;
+Debugger::$productionMode = TRUE;
+header('Content-Type: text/html');
+
+Debugger::enable();
+
+function shutdown() {
+	Assert::same('', ob_get_clean());
+}
+ob_start();
+Debugger::$onFatalError[] = 'shutdown';
