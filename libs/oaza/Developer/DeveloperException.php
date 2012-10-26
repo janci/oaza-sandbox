ebd28355c5970f51231f32eae52d58fb8cb48c27
Jan Svantner <janci@janci.net>
2012-10-26 23:11:58 +0200

    Add missing comments

--- libs/oaza/Developer/DeveloperException.php
+++ libs/oaza/Developer/DeveloperException.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * This file is part of the Oaza Framework (http://nette.org)
+ *
+ * Copyright (c) 2012 Jan Svantner (http://www.janci.net)
+ *
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
+ */
+
+
 namespace Oaza\Developer;
 class DeveloperException  extends \Exception
 {
